f856d7b7e938d6060f0e1e9b8f2238ec30c5d8fc
HongJun Jo <cutehong86@gmail.com>
2025-05-16 19:45:33 +0900

    .

---
+++ OOP/Racing/RacingGame/game.h
@@ -0,0 +1,41 @@
+﻿#pragma once
+#include "race.h"
+#include "training.h"
+#include "UI.h"
+#include <windows.h>
+
+inline void play_game(horse& player) {
+    training trainer(player);
+
+    Sleep(500);
+    system("cls");
+
+    PlaySound(TEXT("BGM2.wav"), NULL, SND_ASYNC | SND_LOOP);
+
+    for (int i = 0; i < MAX_MONTH; i++) {
+        // 티어 계산
+        if (i != 0 && i % 6 == 0) {
+            int tier;
+            if (i <= 12)        tier = 6;
+            else if (i <= 24)   tier = 5;
+            else if (i <= 36)   tier = 4;
+            else if (i <= 48)   tier = 3;
+            else if (i <= 60)   tier = 2;
+            else                tier = 1;
+
+            Race race(player, tier);
+            std::cout << "\n=== 레이스가 시작됩니다! ===\n";
+            race.start();
+            std::cout << "-----------------------레이스 종료-----------------------\n";
+            race.reward();
+        }
+
+        print_stat(i + 1, &player, trainer);
+        Sleep(500);
+        system("cls");
+    }
+
+    std::cout << "\n=== 최종 레이스가 시작됩니다! ===\n";
+    Race race(player, 1);
+    race.start();
+}
---
+++ OOP/Racing/RacingGame/main.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <windows.h>
+#include "horse.h"
+#include "training.h"
+#include "race.h"
+#include "UI.h"
+#include "game.h"
+#include <mmsystem.h> 
+#pragma comment(lib,"winmm.lib")
+
+
+const int startStat = 10.0;
+
+using namespace std;
+
+//void print_basic(int month, horse* player, training& trainer);
+void print_stat(int month, horse* player, training& trainer);
+
+int main() { //cols 가로 , lines 세로 cols=140 lines=30
+    // 콘솔 입출력 인코딩을 UTF-8로 설정
+    SetConsoleOutputCP(CP_UTF8);  // 출력 인코딩
+    SetConsoleCP(CP_UTF8);        // 입력 인코딩
+
+    int start_format;
+    init_console_size(); // 콘솔 크기 초기화
+    //init_game_ui(); // UI 초기화
+    PlaySound(TEXT("BGM1.wav"), NULL, SND_ASYNC | SND_LOOP);
+    start_format = menuDraw();
+    if (start_format == 1)
+    {
+        system("cls");
+    }
+    else if (start_format == 2) {
+        //save&load
+    }
+    else
+        exit(1);
+
+    srand(static_cast<unsigned>(time(nullptr)));
+
+    // 말생성 함수 UI.h에서 처리
+    horse player = select_horse();
+
+    //게임 진행 함수 game,h에서 처리
+    play_game(player);
+    return 0;
+}
+
+void print_stat(int month, horse* player, training& trainer);
